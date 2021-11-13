89392e04040b1934caaa123864091f29e2777432
igor.ko <igor.ko@earnix.com>
2021-11-13 14:37:30 +0200

    commiting changes

---
+++ rolling-deployment/curl-test.sh
@@ -0,0 +1,4 @@
+for i in {1..1035}; do
+   kubectl exec --namespace=default curl -- sh -c 'test=`wget -qO- -T 2  http://webapp-service.default.svc.cluster.local:8080/info 2>&1` && echo "$test OK" || echo "Failed"';
+   echo ""
+done
